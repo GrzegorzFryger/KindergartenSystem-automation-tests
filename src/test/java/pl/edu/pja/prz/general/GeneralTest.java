50696fde013ad8ca48936c0458c62abbc62196e5
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-05-17 20:20:59 +0200

    Add example login unit tests

--- src/test/java/pl/edu/pja/prz/general/GeneralTest.java
+++ src/test/java/pl/edu/pja/prz/general/GeneralTest.java
@@ -3,8 +3,14 @@ package pl.edu.pja.prz.general;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import pl.edu.pja.prz.BaseAutomationTest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+/**
+ * This test suite checks correctness of general application functions
+ */
 public class GeneralTest extends BaseAutomationTest {
 
     @BeforeEach
@@ -20,5 +26,20 @@ public class GeneralTest extends BaseAutomationTest {
     @Test
     public void Should_VisitLoginPage() {
         // Do nothing - initializing web driver starts from login page
+    }
+
+    @Test
+    public void Should_LogIn_When_UserCredentialsAreCorrect() {
+        driver.sendKeysByXPath("//*[@id=\"mat-input-0\"]", "user3@test.com");
+        driver.sendKeysByXPath("//*[@id=\"mat-input-1\"]", "user03");
+        driver.clickElementByXPath("/html/body/app-root/app-login/div/div/mat-card/mat-card-content/div/div[2]/form/div/button/span");
+    }
+
+    @Test
+    public void Should_NotLogIn_When_UserCredentialsAreNotCorrect() {
+        driver.sendKeysByXPath("//*[@id=\"mat-input-0\"]", "bad_email");
+        driver.sendKeysByXPath("//*[@id=\"mat-input-1\"]", "bad_password");
+        driver.clickElementByXPath("/html/body/app-root/app-login/div/div/mat-card/mat-card-content/div/div[2]/form/div/button/span");
+        assertFalse(driver.findElements(By.xpath("/html/body/app-root/app-login/div/div/mat-card/mat-card-content/div/div[2]/form/div/button/span")).isEmpty());
     }
 }
